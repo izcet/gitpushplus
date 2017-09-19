07753217ada033b1041f44052ad266f7e31de191
izcet <isaacjrhett@gmail.com>
2017-09-18 21:15:58 -0700

    [install.sh] [.git_commit_script.sh -> commit_script.sh]

--- install.sh
+++ install.sh
@@ -6,18 +6,27 @@ GPP_PURPLE='\033[0;35m'
 GPP_RED='\033[0;31m'
 GPP_NOCOLOR='\033[0m'
 
-
 G=$GPP_GREEN
 P=$GPP_PURPLE
 R=$GPP_RED
 N=$GPP_NOCOLOR
 
 
+# Initial setup of files
 echo "${G}What directory should the files be stored? (defaults to $P$GPP_DIR$G)$N"
 read RESPONSE
+
 if [ -n "$RESPONSE" ] ; then
 	GPP_DIR=$RESPONSE
 fi
+
+if [ -f $GPP_DIR ] ; then
+	echo "${R}Error: File exists: $P$GPP_DIR$N"
+	exit 1
+fi
+cp ./commit_script.sh $GPP_DIR
+chmod +x ${GPP_DIR}commit_script.sh
+
 
 echo "${G}What .rc file do you use? ($P.bashrc$G, $P.shrc$G, $P.zshrc$G)$N"
 read RC_FILE
@@ -27,10 +36,18 @@ while [ -z $RC_FILE ] ; do
 	read RC_FILE
 done
 
+if [ -d $RC_FILE ] ; then
+	echo "${R}Error: File cannot be a directory: $P$RC_FILE"
+	echo "${G}I bet you think you're really funny.$N"
+	exit 1
+fi
+
 if [ ! -f $RC_FILE ] ; then
 	echo "${P}$RC_FILE$G does not exist. Creating..."
 	touch $RC_FILE
 fi
+
+
 echo "${G}Appending data to $P$RC_FILE$G.$N"
 
 echo "####  Git Push Plus variables and setup  ####\n" >> $RC_FILE
@@ -42,26 +59,4 @@ echo "export GPP_NOCOLOR='\\033[0m'\n" >> $RC_FILE
 echo "source $${GPP_DIR}aliases.sh\n" >> $RC_FILE
 echo "####          End Git Push Plus          ####" >> $RC_FILE
 
-
-
-echo "${W}(OPTIONAL) What is the ${C}github${W} repository url?"
-echo "Press enter to skip..."
-read URL
-if [ ! -z $URL ] ; then
-	echo "${B}Adding remote ${C}gh${B} at ${P}$URL${B}"
-	git remote add gh $URL
-	if [ "$?" -eq 1 ] ; then
-		error "Invalid repository. ${C}gh${R} will have to be manually set."
-		git remote rm gh
-		echo "${B}Remote ${C}gh${B} removed."
-	else
-		git pull gh master 2> /dev/null	
-	fi
-else
-	echo "\033[1A${B}Skipping..."
-fi
-
-if [ ! -z $GIT_DIR ] ; then
-	GIT=$GIT_DIR
-fi
 
